e8279ff6e3edb7f4c343d3e969e3fad5cc4177fa
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-05 13:10:28 +0300

    Checks added(Existence of info-file, correctness). Correctness check should provide more information. Documentation left to be done.

--- Info.py
+++ Info.py
@@ -4,18 +4,38 @@ class InfoCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		infoFileName = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
 
-		infoFile = open(infoFileName)
+		if not os.path.isfile(infoFileName):
+			self.infoFileCreation(infoFileName)
 
-		# if os.system(infoFileCheck):
-		# 	os.system(infoFileCreation)
+		if not (int(self.infoFileCorrectnessCheck(infoFileName)) == -1):
+			print("Reading failed. ", self.infoFileCorrectnessCheck(infoFileName))
+			return 0
 
-		# if os.system(infoFileCorrectionCheck):
-		# 	print("!!!")
-		# 	return 0
+		infoFile = open(infoFileName, 'r')
 
 		lines = infoFile.readlines()
 		n = int(lines[0])
-		print(lines[n])
+		if n != -1:
+			print(lines[n])
+		else:
+			print("No project is active now.")
 
 		infoFile.close()
+
+	def infoFileCreation(self, infoFileName):
+		open(infoFileName, 'w').write("-1")
+
+	def infoFileCorrectnessCheck(self, infoFileName):
+		infoFile = open(infoFileName, 'r')
+		lines = infoFile.readlines()
+		if len(lines[0].split()) > 1:
+			return 0
+		n = int(lines[0])
+		if len(lines) > n + 1 and n != -1:
+			return -2
+		for i in range(1, n):
+			if len(lines[i]) != 2:
+				return i
+		infoFile.close()
+		return -1
 
