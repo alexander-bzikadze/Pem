83c1e568c89c5d1239d65cb9ec1c7d3490ba3328
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-05 16:20:04 +0300

    Creation created. Documentation needs to be done.

--- createProject.py
+++ createProject.py
@@ -3,29 +3,34 @@ import sublime, sublime_plugin, os
 class CreateProjectCommand(sublime_plugin.TextCommand):
 	def run(self, edit, name, path = os.getcwd()):
 		infoFileName = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
-		if not os.path.isfile(infoFileName):
+		if (os.path.isfile(infoFileName) != True) or (os.stat(infoFileName).st_size == 0):
 			self.infoFileCreation(infoFileName)
 
 		if os.path.isfile(os.path.join(path, name) + ".pem"):
 			print("Project path is already busy.")
 			return 0
 
-		# projectFile = open(os.path.join(path, name) + ".pem", 'w')
-		# projectFile.write("project_name = " + name + '\n\n')
-		# projectFile.write("specification:" + "\n\n")
-		# projectFile.write("source:" + "\n\n")
-		# projectFile.close();
+		projectFile = open(os.path.join(path, name) + ".pem", 'w')
+		projectFile.write("project_name = " + name + '\n\n')
+		projectFile.write("specification:" + "\n\n")
+		projectFile.write("source:" + "\n\n")
+		projectFile.close();
 
 		self.addProjectToInfoFile(infoFileName, name, path)
 
 	def addProjectToInfoFile(self, infoFileName, name, path):
-		file = open(infoFileName, "r+")
+		file = open(infoFileName, "r")
 		lines = file.readlines()
-		print(lines)
-		lines[0] = len(lines)
-		lines += name + " " + path
+		file.close()
+
+		lines[0] = str(len(lines)) + "\n"
+		lines.append(str(name + " " + path) + "\n")
+
+		file = open(infoFileName, "w")
 		file.writelines(lines)
 		file.close()
 
 	def infoFileCreation(self, infoFileName):
-		open(infoFileName, 'w').write("-1").close()
+		file = open(infoFileName, 'w')
+		file.write("-1")
+		file.close()
