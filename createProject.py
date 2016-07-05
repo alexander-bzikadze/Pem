ca3aecdf0cbf8701819e9a835266fc81052a26f5
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-05 15:19:08 +0300

    Critical mistakes found in previous branch.

--- createProject.py
+++ createProject.py
@@ -6,13 +6,26 @@ class CreateProjectCommand(sublime_plugin.TextCommand):
 		if not os.path.isfile(infoFileName):
 			self.infoFileCreation(infoFileName)
 
-		projectFile = open(os.path.join(path, name) + ".pem", 'w')
-		projectFile.write("project_name = " + name + '\n\n')
-		projectFile.write("specification:" + "\n\n")
-		projectFile.write("source:" + "\n\n")
-		projectFile.close();
+		if os.path.isfile(os.path.join(path, name) + ".pem"):
+			print("Project path is already busy.")
+			return 0
 
+		# projectFile = open(os.path.join(path, name) + ".pem", 'w')
+		# projectFile.write("project_name = " + name + '\n\n')
+		# projectFile.write("specification:" + "\n\n")
+		# projectFile.write("source:" + "\n\n")
+		# projectFile.close();
 
+		self.addProjectToInfoFile(infoFileName, name, path)
+
+	def addProjectToInfoFile(self, infoFileName, name, path):
+		file = open(infoFileName, "r+")
+		lines = file.readlines()
+		print(lines)
+		lines[0] = len(lines)
+		lines += name + " " + path
+		file.writelines(lines)
+		file.close()
 
 	def infoFileCreation(self, infoFileName):
-		open(infoFileName, 'w').write("-1")
+		open(infoFileName, 'w').write("-1").close()
