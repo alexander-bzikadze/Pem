8539b46598ef2972fe1a843bd88169c6ff923be9
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-07 16:11:27 +0300

    Finally works. Also CT intergrated.

--- openFile.py
+++ openFile.py
@@ -1,36 +1,33 @@
 import sublime, sublime_plugin, os, subprocess
 
+from importlib.machinery import SourceFileLoader
+ct = SourceFileLoader("CorrectnessTests", os.path.join(sublime.packages_path(), "User", "correctnessTests.py")).load_module()
+
 extension = ".pem"
 csextension = ".cs"
 
 class OpenFileCommand(sublime_plugin.TextCommand):
 	def run(self, edit, name):
-		infoFileName = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+		infoFilePath = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+		cT = ct.CorrectnessTests(infoFilePath)
+		cT.infoFileExistence()
 
-		if (os.path.isfile(infoFileName) != True) or (os.stat(infoFileName).st_size == 0):
-			self.infoFileCreation(infoFileName)
-
-		infoFileName = os.path.join(infoFileName)
-		if not os.path.isfile(infoFileName):
-			print("Info-file not found.")
-			return 0
-		if os.stat(infoFileName).st_size == 0:
-			print("Info-file is empty.")
-			return 0
-		infoFile = open(infoFileName, 'r')
+		infoFile = open(infoFilePath, 'r')
 		line = infoFile.readline()
 		infoFile.close()
-		if (len(line.split()) != 3) or (not line.split()[0].isdigit()) or (line.split()[0] == "-1"):
+
+		if cT.projectSelection():
 			print("Project is not selected.")
 			return 0
-		projectName = line.split()[1]
-		projectPath = os.path.join(line.split()[2], projectName + extension)
-		if not (os.path.isfile(projectPath) or os.stat(projectPath).st_size == 0):
+		if cT.projectFileExistence(line):
 			print("Project file not found or it is empty.")
 			return 0
 
+		projectName = line.split()[1]
+		projectPath = os.path.join(line.split()[2], projectName + extension)
 		projectFile = open(projectPath, 'r')
 		lines = projectFile.readlines()
-		if '\t' + name + '\n' in lines:
-			subprocess.call(["subl", os.path.join(line.split()[2]) + "/" + name + csextension])
+		print (lines)
+		if ('\t' + name + '\n' in lines) and (lines.index("source:\n") < lines.index('\t' + name + '\n')):
+			self.view.window().open_file(name + csextension, sublime.ENCODED_POSITION)
 		projectFile.close()
