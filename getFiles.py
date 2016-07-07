703710ba34dd866571b55dd37d325114446a1772
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-07 14:32:05 +0300

    CT integrated.

--- getFiles.py
+++ getFiles.py
@@ -1,41 +1,34 @@
 import sublime, sublime_plugin, os
 
+from importlib.machinery import SourceFileLoader
+ct = SourceFileLoader("CorrectnessTests", os.path.join(sublime.packages_path(), "User", "correctnessTests.py")).load_module()
+
 extension = ".pem"
 
 class GetFilesCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		infoFileName = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+		infoFilePath = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+		cT = ct.CorrectnessTests(infoFilePath)
+		cT.infoFileExistence()
 
-		if (os.path.isfile(infoFileName) != True) or (os.stat(infoFileName).st_size == 0):
-			self.infoFileCreation(infoFileName)
-
-		infoFileName = os.path.join(infoFileName)
-		if not os.path.isfile(infoFileName):
-			print("Info-file not found.")
-			return 0
-		if os.stat(infoFileName).st_size == 0:
-			print("Info-file is empty.")
-			return 0
-		infoFile = open(infoFileName, 'r')
+		infoFile = open(infoFilePath, 'r')
 		line = infoFile.readline()
 		infoFile.close()
-		if (len(line.split()) != 3) or (not line.split()[0].isdigit()) or (line.split()[0] == "-1"):
+
+		if cT.projectSelection():
 			print("Project is not selected.")
 			return 0
-		projectName = line.split()[1]
-		projectPath = os.path.join(line.split()[2], projectName + extension)
-		if not (os.path.isfile(projectPath) or os.stat(projectPath).st_size == 0):
+		if cT.projectFileExistence(line):
 			print("Project file not found or it is empty.")
 			return 0
+		if cT.projectFileCorrectness(line):
+			print("Project file is not correst.")
+			return 0
 
+		projectName = line.split()[1]
+		projectPath = os.path.join(line.split()[2], projectName + extension)
 		projectFile = open(projectPath, 'r')
 		lines = projectFile.readlines()
+		projectFile.close()
 		for i in range(lines.index("source:\n") + 1, len(lines)):
 			print(lines[i][0 : len(lines[i]) - 1])
-		projectFile.close()
-
-
-	def infoFileCreation(self, infoFileName):
-		file = open(infoFileName, 'w')
-		file.write("-1")
-		file.close()
