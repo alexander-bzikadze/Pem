c47637cd06d4ead5c9c0e5176bd2b40f16843a8c
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-07 14:26:04 +0300

    CT intergrated.

--- createProject.py
+++ createProject.py
@@ -1,12 +1,15 @@
 import sublime, sublime_plugin, os
 
+from importlib.machinery import SourceFileLoader
+ct = SourceFileLoader("CorrectnessTests", os.path.join(sublime.packages_path(), "User", "correctnessTests.py")).load_module()
+
 class CreateProjectCommand(sublime_plugin.TextCommand):
 	def run(self, edit, name, path = os.path.expanduser('~/')):
-		infoFileName = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
-		if (os.path.isfile(infoFileName) != True) or (os.stat(infoFileName).st_size == 0):
-			self.infoFileCreation(infoFileName)
+		infoFilePath = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+		cT = ct.CorrectnessTests(infoFilePath)
+		cT.infoFileExistence()
 
-		if os.path.isfile(os.path.join(path, name) + ".pem"):
+		if cT.fileExistence(os.path.join(path, name) + ".pem"):
 			print("Project path is already busy.")
 			return 0
 
@@ -16,21 +19,16 @@ class CreateProjectCommand(sublime_plugin.TextCommand):
 		projectFile.write("source:" + "\n\n")
 		projectFile.close();
 
-		self.addProjectToInfoFile(infoFileName, name, path)
+		self.addProjectToInfoFile(infoFilePath, name, path)
 
-	def addProjectToInfoFile(self, infoFileName, name, path):
-		file = open(infoFileName, "r")
+	def addProjectToInfoFile(self, infoFilePath, name, path):
+		file = open(infoFilePath, "r")
 		lines = file.readlines()
 		file.close()
 
 		lines[0] = str(len(lines)) + " " + str(name + " " + path) + "\n"
 		lines.append(str(name + " " + path) + "\n")
 
-		file = open(infoFileName, "w")
+		file = open(infoFilePath, "w")
 		file.writelines(lines)
-		file.close()
-
-	def infoFileCreation(self, infoFileName):
-		file = open(infoFileName, 'w')
-		file.write("-1")
 		file.close()
