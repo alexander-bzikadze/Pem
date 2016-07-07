e052c80a4d6d29a086535bab87b4d596ef4c3059
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-07 14:02:59 +0300

    CT added, they've been modified.

--- addFile.py
+++ addFile.py
@@ -1,6 +1,9 @@
 import sublime, sublime_plugin, os
 import subprocess
 
+from importlib.machinery import SourceFileLoader
+ct = SourceFileLoader("CorrectnessTests", os.path.join(sublime.packages_path(), "User", "correctnessTests.py")).load_module()
+
 extension = ".pem"
 csextension = ".cs"
 project_name = "project_name = "
@@ -9,42 +12,42 @@ specification = "specification:"
 
 class AddFileCommand(sublime_plugin.TextCommand):
 	def run(self, edit, name):
-		infoFileName = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+		infoFilePath = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+		cT = ct.CorrectnessTests(infoFilePath)
+		cT.infoFileExistence()
 
-		if (os.path.isfile(infoFileName) != True) or (os.stat(infoFileName).st_size == 0):
-			self.infoFileCreation(infoFileName)
-
-		if not os.path.isfile(infoFileName):
-			print("Info-file not found.")
-			return 0
-		if os.stat(infoFileName).st_size == 0:
-			print("Info-file is empty.")
-			return 0
-		infoFile = open(infoFileName, 'r')
+		infoFile = open(infoFilePath, 'r')
 		line = infoFile.readline()
 		infoFile.close()
-		if (len(line.split()) != 3) or (not line.split()[0].isdigit()) or (line.split()[0] == "-1"):
+
+
+		if cT.projectSelection():
 			print("Project is not selected.")
 			return 0
-		projectName = line.split()[1]
-		projectPath = os.path.join(line.split()[2], projectName + extension)
-		if not (os.path.isfile(projectPath) or os.stat(projectPath).st_size == 0):
+
+		if cT.projectFileExistence(line):
 			print("Project file not found or it is empty.")
 			return 0
-		filePath = os.path.join(line.split()[2], name + csextension)
 
-		if os.path.isfile(filePath):
+		if cT.projectFileCorrectness(line):
+			print("Project file is not correct.")
+			return 0
+
+		if cT.fileExistence(line):
 			print("File already exists.")
 			return 0
 
+		projectName = line.split()[1]
+
+		filePath = os.path.join(line.split()[2], name + csextension)
 		file = open(filePath, 'w')
 		namespace = projectName
-
 		file.write("using System;\nusing System.Collections.Generic;\n")
 		file.write("\n\n")
 		file.write("namespace " + namespace + "\n{\n\tpublic class " + "".join(name) + "\n\t{\n\t\t\n\t}\n}")
 		file.close()
 
+		projectPath = os.path.join(line.split()[2], projectName + extension)
 		projectFile = open(projectPath, 'r')
 		lines = projectFile.readlines()
 		projectFile.close()
@@ -53,9 +56,3 @@ class AddFileCommand(sublime_plugin.TextCommand):
 		projectFile.writelines(lines)
 		projectFile.close()
 		return 0
-
-
-	def infoFileCreation(self, infoFileName):
-		file = open(infoFileName, 'w')
-		file.write("-1")
-		file.close()
