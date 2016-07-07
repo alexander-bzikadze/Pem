185018c13dfa24aed4d7d8a00dc90d5ccd7341b0
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-07 14:06:45 +0300

    CT modified.

--- correctnessTests.py
+++ correctnessTests.py
@@ -5,35 +5,33 @@ import os
 extension = ".pem"
 
 class CorrectnessTests:
-	__infoFilePath = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
+	def __init__(self, infoFilePath):
+		self.__infoFilePath = infoFilePath
 
-	@staticmethod
+	__infoFilePath = " "
+
 	def infoFileExistence(self):
 		if (os.path.isfile(self.__infoFilePath) != True) or (os.stat(self.__infoFilePath).st_size == 0):
 			self.infoFileCreation(self.__infoFilePath)
 			return 1
 		return 0
 
-	@staticmethod
 	def __infoFileCreation(self):
 		infoFile = open(self.__infoFilePath, 'w')
 		infoFile.write("-1")
 		infoFile.close()
 
-	@staticmethod
 	def projectSelection(self):
 		infoFile = open(self.__infoFilePath, 'r')
 		line = infoFile.readline()
 		infoFile.close()
-		return not ((len(line.split()) == 3) and (line.split()[0].isdigit()) and (line.split()[0] == "-1"))
+		return not ((len(line.split()) == 3) and (line.split()[0].isdigit()) and (int(line.split()[0]) > 0))
 
-	@staticmethod
 	def projectFileExistence(self, line):
 		projectName = line.split()[1]
 		projectPath = os.path.join(line.split()[2], projectName + extension)
 		return not (os.path.isfile(projectPath) or os.stat(projectPath).st_size == 0)
 
-	@staticmethod
 	def projectFileCorrectness(self, line):
 		projectName = line.split()[1]
 		projectPath = os.path.join(line.split()[2], projectName + extension)
@@ -42,17 +40,15 @@ class CorrectnessTests:
 		projectFile.close()
 		if not "project_name = " in lines[0]:
 			return 1
-		elif not "specifications:\n" in lines:
+		elif not "specification:\n" in lines:
 			return 2
 		elif not "source:\n" in lines:
 			return 3
 		return 0
 
-	@staticmethod
 	def fileExistence(self, filePath):
 		return os.path.isfile(filePath)
 
-	@staticmethod
 	def infoFileCorrectnessLite(self):
 		infoFile = open(self.__infoFilePath, 'r')
 		lines = infoFile.readlines()
@@ -74,7 +70,6 @@ class CorrectnessTests:
 			return 2
 		return 4
 
-	@staticmethod
 	def infoFileCorrectnessHard(self):
 		infoFile = open(self.__infoFilePath, 'r')
 		lines = infoFile.readlines()
@@ -95,5 +90,3 @@ class CorrectnessTests:
 		if (len(projectsThatDoesNotExist)):
 			return 6
 		
-
-
