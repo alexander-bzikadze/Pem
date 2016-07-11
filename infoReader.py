49a9ba97b3744e1c7e607365a53855702b678d61
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-11 12:31:55 +0300

    Exception added.

--- infoReader.py
+++ infoReader.py
@@ -4,6 +4,7 @@ import os
 
 from importlib.machinery import SourceFileLoader
 ct = SourceFileLoader("CorrectnessTests", os.path.join(sublime.packages_path(), "User", "correctnessTests.py")).load_module()
+Exceptions = SourceFileLoader("Exceptions", os.path.join(sublime.packages_path(), "User", "projectNotSelectedException.py")).load_module()
 
 class InfoReader:
 	__infoFileName = "Info.txt"
@@ -22,15 +23,20 @@ class InfoReader:
 		lines = infoFile.readlines()
 		infoFile.close()
 
-		self.__currecntProject = line[0].split()[1]
-		self.__currecntProjectPath = line[0].split()[2]
+		if line.split() != 1:
+			self.__currecntProject = line[0].split()[1]
+			self.__currecntProjectPath = line[0].split()[2]
 		self.__projects = [line.split()[1] for line in lines]
 		self.__projectPaths = [line.split()[2] for line in lines]
 
 	def getCurrecntProject(self):
+		if __currecntProject == " ":
+			raise projectNotSelectedException("No project selected.")
 		return self.__currecntProject
 
 	def getCurrentProjectPath(self):
+		if __currecntProjectPath == " ":
+			raise projectNotSelectedException("No project selected.")
 		return self.__currecntProjectPath
 
 	def getProjects(self):
