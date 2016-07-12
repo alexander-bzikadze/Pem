6e85e170fc8c9bb2e5fd9234a9b1b5a910394481
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-12 17:01:22 +0300

    Now it compiles

--- compileProj.py
+++ compileProj.py
@@ -1,35 +1,42 @@
 import sublime, sublime_plugin, os
-import subprocess
+import subprocess, sys
 
 from importlib.machinery import SourceFileLoader
 ct = SourceFileLoader("CorrectnessTests", os.path.join(sublime.packages_path(), "User", "correctnessTests.py")).load_module()
+ir = SourceFileLoader("InfoReader", os.path.join(sublime.packages_path(), "User", "infoReader.py")).load_module()
+pr = SourceFileLoader("ProjectReader", os.path.join(sublime.packages_path(), "User", "projectReader.py")).load_module()
 
-extension = ".pem"
 csextension = ".cs"
-project_name = "project_name = "
-source = "source:"
-specification = "specification:"
 
-class AddFileCommand(sublime_plugin.TextCommand):
+class CompileProjectCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		infoFilePath = os.path.join(sublime.packages_path(), "User", "Pem", "Info.txt")
-		cT = ct.CorrectnessTests(infoFilePath)
+		info = ir.InfoReader()
+		cT = ct.CorrectnessTests()
 		cT.infoFileExistence()
-
-		infoFile = open(infoFilePath, 'r')
-		line = infoFile.readline()
-		infoFile.close()
 
 		if cT.projectSelection():
 			print("Project is not selected.")
 			return 0
-
-		if cT.projectFileExistence(line):
+		if cT.projectFileExistence(info.getCurrentProject(), info.getCurrentProjectPath()):
 			print("Project file not found or it is empty.")
 			return 0
-
-		if cT.projectFileCorrectness(line):
+		if cT.projectFileCorrectness(info.getCurrentProject(), info.getCurrentProjectPath()):
 			print("Project file is not correct.")
 			return 0
 
-		projectName = line.split()[1]
+		projectReader = pr.ProjectReader()
+		makefile = open(os.path.join(info.getCurrentProjectPath(), "Makefile"), 'w')
+		makefile.write("all:\n")
+		makefile.write("\tcd " + info.getCurrentProjectPath() + '\n')
+		makefile.write("\tmcs")
+		for file in projectReader.getSource():
+			makefile.write(" " + file + csextension)
+		makefile.write(" -target:exe -out:" + info.getCurrentProject() + ".exe\n")
+		makefile.write('\n')
+		makefile.close()
+		make_process = subprocess.Popen(["make", "-C", info.getCurrentProjectPath()], stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+		print(make_process.communicate())
+		os.remove(os.path.join(info.getCurrentProjectPath(), "Makefile"))
+
+
+		
