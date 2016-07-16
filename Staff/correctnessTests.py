54ec4cf554041f653834c967b21c77f6e90f9cf2
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2016-07-16 15:37:25 +0300

    Kernel documentation added. Also added a new type of exception and raised it in ProjectReader. Type is ProjectFileDoesNotExistException.

--- Staff/correctnessTests.py
+++ Staff/correctnessTests.py
@@ -2,20 +2,26 @@
 
 import sublime, sublime_plugin, os
 
+# List of tests to get needed kind of correctness.
+# For now on, if test returns 0 - it went correctly, otherwise - incorrectly.
+
 extension = ".pem"
 
 class CorrectnessTests:
 	__infoFilePath = ""
 
+	# Constructor finds way to infofile.
 	def __init__(self):
 		self.__infoFilePath = os.path.join(sublime.packages_path(), "Pem", "Info.txt")
 
+	# Checks infofile for existence and emptiness. If check failes, creates standart infofile.
 	def infoFileExistence(self):
 		if (os.path.isfile(self.__infoFilePath) != True) or (os.stat(self.__infoFilePath).st_size == 0):
 			self.__infoFileCreation()
 			return 1
 		return 0
 
+	# Creates standart infofile, that consists of "-1\n".
 	def __infoFileCreation(self):
 		if not os.path.exists(os.path.join(sublime.packages_path(), "Pem")):
 			os.makedirs(os.path.join(sublime.packages_path(), "Pem"))
@@ -23,18 +29,21 @@ class CorrectnessTests:
 		infoFile.write("-1\n")
 		infoFile.close()
 
+	# Checks if a project is selected in infofile.
 	def projectSelection(self):
 		infoFile = open(self.__infoFilePath, 'r')
 		line = infoFile.readline()
 		infoFile.close()
 		return not ((len(line.split()) == 3) and (line.split()[0].isdigit()) and (int(line.split()[0]) > 0))
 
+	# Checks if a projectfile existes in given directory by given name. Goes correctly if no PF existes.
 	def projectFileExistence(self, projectName, projectDir):
 		projectPath = os.path.join(projectDir, projectName + extension)
 		if os.path.isfile(projectPath):
 			return os.stat(projectPath).st_size == 0
 		return not (os.path.isfile(projectPath))
 
+	# Checks project-file for correctness. It is correct, if there is "project_name = ", "specification" and "source" lines in it.
 	def projectFileCorrectness(self, projectName, projectDir):
 		projectPath = os.path.join(projectDir, projectName + extension)
 		projectFile = open(projectPath, 'r')
@@ -48,9 +57,12 @@ class CorrectnessTests:
 			return 3
 		return 0
 
+	# Standart check for selected file existence. Goes correctly if file does exist.
 	def fileExistence(self, fileName, filePath):
 		return os.path.isfile(os.path.join(filePath, fileName))
 
+	# Check for infofile correctness. Does not check all the infofile.
+	# Checks first line for correctness and file till the current selected project.
 	def infoFileCorrectnessLite(self):
 		infoFile = open(self.__infoFilePath, 'r')
 		lines = infoFile.readlines()
@@ -68,6 +80,8 @@ class CorrectnessTests:
 			return 2
 		return 4
 
+	# Checks infofile for correctness. Runs previous test. Then checks all the project contained.
+	# Checks lines for correctness, then checks, that project are not a myth.
 	def infoFileCorrectnessHard(self):
 		infoFile = open(self.__infoFilePath, 'r')
 		lines = infoFile.readlines()
